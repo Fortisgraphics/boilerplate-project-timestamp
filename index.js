676c68f0a85726c61d5523b365f3a2882fa1e232
Tandu Yanmick Ateh <95031570+Fortisgraphics@users.noreply.github.com>
2023-10-18 16:29:54 +0000

    Update index.js with my solution, at first API endpoint...

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
-// index.js
-// where your node app starts
+// // here your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const invalidDate = (date) => date.toUTCString() === "Invalid Date";
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/:date", function (req, res) {
+  const { date } = req.params;
+  let currentDate = new Date(date);
+
+  if (invalidDate(currentDate)) {
+    currentDate = new Date(+date);
+  }
+
+  if (invalidDate(currentDate)) {
+    res.json({
+      error: "Invalid Date",
+    });
+    return;
+  }
+
+  res.json({
+    unix: currentDate.getTime(),
+    utc: currentDate.toUTCString(),
+  });
 });
 
-
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
